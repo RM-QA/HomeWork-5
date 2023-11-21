e4b80b4f1fb70a63bdc9857b21da173b0b8dcfed
Rehab <rehabmostafa.j@gmail.com>
2023-11-22 01:12:10 +0200

    class 12 practice code

--- src/test/java/org/example/ApiMockTest.java
+++ src/test/java/org/example/ApiMockTest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.google.gson.Gson;
+import dto.OrderDtoTest;
 import org.apache.http.HttpStatus;
 
 import org.junit.jupiter.api.Test;
@@ -8,25 +10,38 @@ import static io.restassured.RestAssured.given;
 
 public class ApiMockTest {
     @Test
-    public void testWithBody(){
-        String requestBody = "{\n" +
-                "  \"status\": \"OPEN\",\n" +
-                "  \"courierId\": 0,\n" +
-                "  \"customerName\": \"string\",\n" +
-                "  \"customerPhone\": \"string\",\n" +
-                "  \"comment\": \"string\",\n" +
-                "  \"id\": 0\n" +
-                "}";
+    public void testWithBody() {
+//        String requestBody = "{\n" +
+//                "  \"status\": \"OPEN\",\n" +
+//                "  \"courierId\": 0,\n" +
+//                "  \"customerName\": \"string\",\n" +
+//                "  \"customerPhone\": \"string\",\n" +
+//                "  \"comment\": \"string\",\n" +
+//                "  \"id\": 0\n" +
+//                "}";
+
+        // dto creation with constructor
+        OrderDtoTest orderDtoTest = new OrderDtoTest("name", "123456", "comment");
+
+        // dto creation with constructor by default and setter.
+        OrderDtoTest orderDtoTestSetter = new OrderDtoTest();
+        orderDtoTestSetter.setComment("new comment");
+        orderDtoTestSetter.setCustomerPhone("21546212");
+        orderDtoTestSetter.setCustomerName("Name");
+
+        //serialization
+        String orderDtoTestAsJson = new Gson().toJson(orderDtoTest);
+
         given().
-                 header(" Content-Type:", "application/json")
-                .body(requestBody)
-                 .log()
-                 .all()
+                header(" Content-Type:", "application/json")
+                .body(orderDtoTest)
+                .log()
+                .all()
                 .post(" http://51.250.6.164:8080/test-orders")
                 .then()
-                 .log()
-                 .all()
-                .statusCode(HttpStatus.SC_OK);
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
 
     }
 
